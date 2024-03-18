43f3a860ff6bfd0055b9a3263d49c2fece57f53a
TheQuestioningCake <perrywayne39@gmail.com>
2024-03-18 19:51:33 -0400

    adding ant's code

--- search-result.js
+++ search-result.js
@@ -48,3 +48,13 @@ backToHomePageBtn.addEventListener("click", function() {
     // Load another HTML file
     window.location.href = "index.html";
 });
+
+const openModalBtn = document.getElementById('openModal');
+const closeModalBtn = document.getElementById('closeModal');
+const modal = document.getElementById('modal');
+openModalBtn.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+});
+closeModalBtn.addEventListener('click', () => {
+    modal.classList.add('hidden');
+});
